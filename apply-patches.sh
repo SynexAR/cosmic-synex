1801a789f2208263d85d364fe2f4253928bcaadb
Bruno Ernesto Russo <root@synex.ar>
2026-09-16 00:26:15 -0300

    Bump to COSMIC epoch-1.8.0 (tag and package version)

--- apply-patches.sh
+++ apply-patches.sh
@@ -13,7 +13,7 @@
 set -eu
 
 # --- Configuration ---
-COSMIC_TAG="epoch-1.5.0"
+COSMIC_TAG="epoch-1.8.0"
 COSMIC_REPO="https://github.com/pop-os/cosmic-epoch"
 PATCHES_DIR="$(cd "$(dirname "$0")" && pwd)/patches"
 WORK_DIR="${1:-$(pwd)}"
